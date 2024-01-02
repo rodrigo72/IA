abc289ee82fd4b5a5ecfb37264b18641b19776f8
Digaz <a100646@alunos.uminho.pt>
2024-01-02 22:05:27 +0000

    Main retirada do menu

--- src/Menu.py
+++ src/Menu.py
@@ -2,9 +2,6 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 import Cidade as c
-import Estafeta as est
-import Encomenda as enc
-import Grafo as g
 
 
 class Menu:
@@ -238,31 +235,3 @@ class Menu:
         canvas.pack(pady=100)
 
         canvas.imagem = imagem
-
-
-def main():
-    mapa = c.inicializa_mapa()
-    encomendas = enc.inicializa_encomendas(mapa)
-    grafo = g.Grafo()
-    grafo.inicializa_grafo()
-    estafetas = est.inicializa_estafetas()
-
-    est.distribuir_encomendas(encomendas, estafetas)
-
-    est.entrega(grafo.nodos, estafetas)
-
-    root = tk.Tk()
-    menu = Menu(root, mapa)
-
-    menu.adicionar_menu("Principal", lambda: menu.principal(menu))
-    menu.adicionar_menu("App", lambda: menu.app(menu))
-    menu.adicionar_menu("Estafetas", lambda: menu.estafetas_info(menu, estafetas))
-    menu.adicionar_menu("Encomendas", lambda: menu.encomendas_info(mapa, menu, encomendas))
-
-    menu.abrir_menu("Principal")
-
-    root.mainloop()
-
-
-if __name__ == "__main__":
-    main()
